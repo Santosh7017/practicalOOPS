f7a89a2ff713131af621937a25f1a3da791cb302
Santosh7017 <santoshmorya400@gmail.com>
2021-11-27 22:17:47 +0530

    custom exception handling

--- src/practicalL/ThrowableException.java
+++ src/practicalL/ThrowableException.java
@@ -1,7 +1,23 @@
 package practicalL;
-
+class NotFound extends Exception{
+    public NotFound(String str){
+        super(str);
+    }
+}
 public class ThrowableException {
-    public static void main(String[] args){
+    public static void main(String[] args) throws NotFound{
+        int a = 1234567890;
+if (a<1){
+    System.out.println("hello world");
+}
+        try {
+                throw new NotFound("no answer found");
+
+
+        } catch (NotFound notFound) {
+            notFound.printStackTrace();
+        }
+
 
     }
 }
