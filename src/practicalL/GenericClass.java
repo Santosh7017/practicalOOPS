e66f905a9a1a2558112fbf0e74ff599f36a7f01f
Santosh7017 <santoshmorya400@gmail.com>
2021-12-03 02:42:44 +0530

    Pattern printing using Generic class and method

--- src/practicalL/GenericClass.java
+++ src/practicalL/GenericClass.java
@@ -12,7 +12,7 @@ class Abc<T> {
 //        this.t = t;
 
 
-//    }
+    //    }
     public T getT() {
         return t;
     }
@@ -20,23 +20,30 @@ class Abc<T> {
     public void setT(T t) {
         this.t = t;
     }
-}
 
-public class GenericClass {
+    public void pattern() {
 
-    public static void main(String [] args){
-        Scanner sc = new Scanner(System.in);
-
-      //   Abc <Integer> obj = new <Integer> Abc(76);
-        Abc  <Integer> obj1 = new <Integer>Abc();
-        Abc  <String> obj2 = new <String>Abc();
-         obj2.setT(sc.next());
-        for(int i =0;i<10;i++){
-            for (int j= 0;j<i;j++){
-                System.out.print(obj2.getT()+" ");
-            }
+        for (int i = 0; i < 10; i++) {
+                    for(int j = 0;j<i;j++){
+                        System.out.print(getT()+" ");
+                    }
             System.out.println("");
         }
-
     }
 }
+
+    public class GenericClass {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            //   Abc <Integer> obj = new <Integer> Abc(76);
+            Abc<Integer> obj1 = new <Integer>Abc();
+            Abc<String> obj2 = new <String>Abc();
+            obj1.setT(123);
+            obj1.pattern();
+
+
+        }
+    }
+
