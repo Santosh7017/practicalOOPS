34f99b1d1cfbea8b8f0fa35623893aa6c9390e48
Santosh Kumar MOrya <santoshmorya400@gmail.com>
2021-12-10 02:47:48 +0530

    ArrayList lab

--- src/practicalL/ArrayListL.java
+++ src/practicalL/ArrayListL.java
@@ -1,37 +1,71 @@
 package practicalL;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class ArrayListL {
     public static void main(String[] args) {
-                ArrayList<Integer> al=new ArrayList<Integer>();
-                al.add(67);
-                al.add(52);
-                al.add(54);
-                ArrayList<Integer> al2=new ArrayList<Integer>();
-                al2.addAll(al);
-                al2.add(98);
-                al2.add(66);
-                al.add(23);
-                System.out.println(al); //[67, 52, 54, 23]
-                System.out.println(al2); //[67, 52, 54, 98, 66]
-                System.out.println(al2.retainAll(al)); //true
-                System.out.println(al2); //[67, 52, 54]
-                al.remove(new Integer(52));
-                System.out.println(al); //[67, 54, 23]
-                System.out.println(al.size()); //3
-                al2.add(21);
-                System.out.println(al2.size()); //4
-                System.out.println(al); //[67, 54, 23]
-                System.out.println(al2);  //[67, 52, 54, 21]
-                al.removeAll(al2);
-                System.out.println(al); //[23]
-                al2.clear();
-                System.out.println(al2); //[]
-                System.out.println(al.contains(new Integer(87))); //false
-                System.out.println(al.contains(new Integer(23))); //true
-                System.out.println(al.isEmpty()); //false
-                System.out.println(al2.isEmpty()); //true
 
+                 ArrayList<Integer> al=new ArrayList<Integer>();
+//                al.add(76);
+//                 al.add(67);
+//                al.add(52);
+//                al.add(54);
+//                al.add(87);
+//                al.add(76);
+
+                ArrayList<String> al2=new ArrayList<String>();
+
+//                al2.add(98);
+//                al2.add(98);
+//                al2.add(51);
+        al2.add("santosh");
+
+                al.add(23);
+                al.add(54);
+                al.add(51);
+
+                int a = al.get(1);
+                String s = al2.get(0);
+                System.out.println(a);
+//                al.removeAll(al2);
+
+//        System.out.println(al);
+//        System.out.println(al2);
+//        System.out.println(al);
+//        al.clear();
+////        al2.remove(new Integer(98));
+////        al2.retainAll(al);
+//        System.out.println(al);
+//        System.out.println(al2);
+
+//                System.out.println(al); //[67, 52, 54, 23]
+//                System.out.println(al2); //[67, 52, 54, 98, 66]
+//                System.out.println(al2.retainAll(al)); //true
+//                System.out.println(al2); //[67, 52, 54]
+//                al.remove(new Integer(52));
+//                System.out.println(al); //[67, 54, 23]
+//                System.out.println(al.size()); //3
+//                al2.add(21);
+//                System.out.println(al2.size()); //4
+//                System.out.println(al); //[67, 54, 23]
+//                System.out.println(al2);  //[67, 52, 54, 21]
+//                al.removeAll(al2);
+//                System.out.println(al); //[23]
+//                al2.clear();
+//                System.out.println(al2); //[]
+//                System.out.println(al.contains(new Integer(87))); //false
+//                System.out.println(al.contains(new Integer(23))); //true
+//                System.out.println(al.isEmpty()); //false
+//                System.out.println(al2.isEmpty()); //true
+//        al.add(0,32);
+//        System.out.println(al); //[32, 23]
+//        al.set(0,76);
+//        System.out.println(al); //[76, 23]
+//        System.out.println(al.get(1)); //23
+//        System.out.println(al.indexOf(76)); //0
+//        System.out.println(al.lastIndexOf(54)); //0
+//        System.out.println(al.indexOf(87)); //-1
+//        System.out.println(al.lastIndexOf(99)); //-1
 
     }
 }
