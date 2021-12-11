19650a8233c9d7e63570cf96364f4db53a98dac1
Santosh Kumar MOrya <santoshmorya400@gmail.com>
2021-12-11 22:23:08 +0530

    JDBC

--- src/practicalL/ExceptionHandling.java
+++ src/practicalL/ExceptionHandling.java
@@ -5,16 +5,22 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Scanner;
 public class ExceptionHandling {
-    public static void main(String[] args){
-        int c = 0;
-try {
-    Scanner sc = new Scanner(System.in);
-    int d = sc.nextInt();
-     c = c*d;
-    System.out.println("Succesfully done");
-}catch (InputMismatchException h){
-    System.out.println("Not good format of integer");
-}
+    public static void main(String[] args) throws Exception {
+        int c = 8;
+
+        try {
+            Scanner sc = new Scanner(System.in);
+            int d = sc.nextInt();
+            c = c / d;
+            System.out.println("Succesfully done");
+        } catch (InputMismatchException h) {
+            System.out.println("Not good format of integer");
+            throw new Exception("Can not device by 0");
+        }
+        catch (Exception e){
+            throw new Exception("Can not device by 0");
         }
     }
+}
+
 
