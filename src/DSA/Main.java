f613cd6a07a6b86e604af744f4dca83713d7e8d1
Santosh Morya <santoshmorya400@gmail.com>
2022-02-27 08:59:00 +0530

    Recursion

---
+++ src/DSA/Main.java
@@ -0,0 +1,22 @@
+package DSA;
+
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+class Test{
+    public void sum(int num){
+        num = 5;
+        System.out.println(num);
+
+    }
+
+
+        }
+public class Main extends  Test {
+    int data;
+    void disp(int temp){
+        data= temp;
+    }
+    public static void main(String[] args) {
+
+    }
+}
