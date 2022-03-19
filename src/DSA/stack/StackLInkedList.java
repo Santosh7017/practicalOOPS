b577978f6b85a53a69ef9a8ad3a0d241f7a1626c
Santosh Morya <santoshmorya400@gmail.com>
2022-03-20 00:35:03 +0530

    Stack using LinkedList

---
+++ src/DSA/stack/StackLInkedList.java
@@ -0,0 +1,13 @@
+package DSA.stack;
+
+public class StackLInkedList {
+    class Node{
+        Node next = null;
+        Node prev;
+        int data;
+        Node(int data){
+            this.data = data;
+        }
+
+    }
+}
