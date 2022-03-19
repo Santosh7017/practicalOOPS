cc09b44355206f992e8106451ab654afbd8e96ef
Santosh Morya <santoshmorya400@gmail.com>
2022-03-20 00:34:16 +0530

    ArrayList

---
+++ src/DSA/ArrayList/MyArraylist.java
@@ -0,0 +1,68 @@
+package DSA.ArrayList;
+
+import java.util.ArrayList;
+
+public class MyArraylist {
+//    class Node{
+//        int data;
+//        Node next = null;
+//        Node prev;
+//        Node(int data){
+//            this.data  = data;
+//        }
+//
+//    }
+//    Node head;
+//
+//    public void addFirst(int data){
+//        Node newNode = new Node(data);
+//        if(head == null){
+//            head =  newNode;
+//            return;
+//        }
+//        newNode.next = head;
+//        head = newNode;
+//
+//    }
+//    public void addLast(int data){
+//        Node newNode = new Node(data);
+//        if(head == null){
+//            head = newNode;
+//            return;
+//        }
+//        Node current = head;
+//
+//        while(current.next!= null){
+//            current = current.next;
+//        }
+//        current.next = newNode;
+//        newNode.next = null;
+//
+//    }
+//    public void print(){
+//        if(head == null){
+//            System.out.println("List is empty");
+//            return;
+//        }
+//        Node current = head;
+//        while(current != null){
+//            System.out.println(current.data);
+//            current = current.next;
+//        }
+//    }
+
+
+
+
+    public static void main(String[] args){
+//        MyArraylist ar = new MyArraylist();
+//        ar.addFirst(1);
+//        ar.addFirst(2);
+//        ar.addFirst(5);
+//
+//        ar.print();
+
+
+    }
+
+}
